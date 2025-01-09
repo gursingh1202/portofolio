01dae3c55b6b5891d828195a688f24fcf86570fc
gursingh1202 <142184366+gursingh1202@users.noreply.github.com>
2025-01-09 17:49:49 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-// script.js
 
-document.getElementById("hamburgerMenu").addEventListener("click", function() {
-  const navMenu = document.getElementById("navMenu");
-  navMenu.classList.toggle("active");
-});
