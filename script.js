66f87c6409a3f2ad0dab76b3045053be4a8840d1
gursingh1202 <142184366+gursingh1202@users.noreply.github.com>
2025-01-09 17:28:03 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+document.getElementById("hamburgerMenu").addEventListener("click", function() {
+  const navMenu = document.getElementById("navMenu");
+  navMenu.classList.toggle("active");
+});
